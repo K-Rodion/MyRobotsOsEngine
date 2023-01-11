31e38598760ee0928c50438b6469dbb2a425a0a6
K-Rodion <kovadlo.r@mail.ru>
2023-01-11 22:47:13 +0300

    Доработка робота FrontRunner

--- OsEngine/Robots/FrontRunner/Model/FrontRunnerBot.cs
+++ OsEngine/Robots/FrontRunner/Model/FrontRunnerBot.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using OsEngine.Entity;
 using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Robots.FrontRunner.ViewModel;
 
 namespace OsEngine.Robots.FrontRunner.Model
 {
@@ -12,6 +14,63 @@ namespace OsEngine.Robots.FrontRunner.Model
     {
         public FrontRunnerBot(string name, StartProgram startProgram) : base(name, startProgram)
         {
+            TabCreate(BotTabType.Simple);
+
+            _tab = TabsSimple[0];
+
+            _tab.MarketDepthUpdateEvent += _tab_MarketDepthUpdateEvent;
+        }
+
+        
+
+        #region Fields========================================================================
+
+        public decimal BigVolume;
+
+        public int Offset;
+
+        public decimal Take;
+
+        public decimal Lot;
+
+        public Position Position = null;
+
+        private BotTabSimple _tab;
+
+        private bool _stateBid = true;
+
+        #endregion
+
+        #region Methods =====================================================================
+
+        private void _tab_MarketDepthUpdateEvent(MarketDepth marketDepth)
+        {
+            if (marketDepth.SecurityNameCode != _tab.Securiti.Name)
+            {
+                return;
+            }
+
+            for (int i = 0; i < marketDepth.Asks.Count; i++)
+            {
+                if (marketDepth.Asks[i].Ask >= BigVolume && Position == null)
+                {
+                    decimal price = marketDepth.Asks[i].Price - Offset * _tab.Securiti.PriceStep;
+
+
+                    _tab.SellAtLimit(Lot, price);
+                }
+            }
+
+            for (int i = 0; i < marketDepth.Bids.Count; i++)
+            {
+                if (marketDepth.Bids[i].Bid >= BigVolume && Position == null)
+                {
+                    decimal price = marketDepth.Bids[i].Price + Offset * _tab.Securiti.PriceStep;
+
+
+                    _tab.BuyAtLimit(Lot, price);
+                }
+            }
         }
 
         public override string GetNameStrategyType()
@@ -21,7 +80,9 @@ namespace OsEngine.Robots.FrontRunner.Model
 
         public override void ShowIndividualSettingsDialog()
         {
-            
+
         }
+
+        #endregion
     }
 }
--- OsEngine/Robots/FrontRunner/ViewModel/VM.cs
+++ OsEngine/Robots/FrontRunner/ViewModel/VM.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using OsEngine.Commands;
 using OsEngine.Robots.FrontRunner.Model;
+
 
 namespace OsEngine.Robots.FrontRunner.ViewModel
 {
@@ -11,7 +14,120 @@ namespace OsEngine.Robots.FrontRunner.ViewModel
     {
         public VM(FrontRunnerBot bot)
         {
-
+            _bot = bot;
         }
+
+        #region Fields========================================================================
+
+        private FrontRunnerBot _bot;
+
+        #endregion
+
+        #region Propetries ===================================================================
+
+        public decimal BigVolume
+        {
+            get => _bot.BigVolume;
+
+            set
+            {
+                _bot.BigVolume = value;
+                OnPropertyChanged(nameof(BigVolume));
+            }
+        }
+        
+
+        public int Offset
+        {
+            get => _bot.Offset;
+
+            set
+            {
+                _bot.Offset = value;
+                OnPropertyChanged(nameof(Offset));
+            }
+        }
+        
+
+        public decimal Take
+        {
+            get => _bot.Take;
+
+            set
+            {
+                _bot.Take = value;
+                OnPropertyChanged(nameof(Take));
+            }
+        }
+       
+
+        public decimal Lot
+        {
+            get => _bot.Lot;
+
+            set
+            {
+                _bot.Lot = value;
+                OnPropertyChanged(nameof(Lot));
+            }
+        }
+       
+
+        public Edit Edit
+        {
+            get => _edit;
+
+            set
+            {
+                _edit = value;
+                OnPropertyChanged(nameof(Edit));
+            }
+        }
+        private Edit _edit;
+
+        #endregion
+
+        #region Commands =====================================================================
+
+        private DelegateCommand _commandStart;
+
+        public ICommand CommandStart
+        {
+            get
+            {
+                if (_commandStart == null)
+                {
+                    _commandStart = new DelegateCommand(Start);
+                }
+
+                return _commandStart;
+            }
+        }
+
+
+        #endregion
+
+        #region Methods =====================================================================
+
+        private void Start(object obj)
+        {
+            if (Edit == Edit.Start)
+            {
+                Edit = Edit.Stop;
+            }
+            else
+            {
+                Edit = Edit.Start;
+            }
+        }
+
+        #endregion
+
+    }
+
+    public enum Edit
+    {
+        Start,
+        Stop
     }
 }
